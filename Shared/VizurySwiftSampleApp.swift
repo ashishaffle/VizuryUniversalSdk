2f4172e8d70d8a026b5a4a4418a6f31febb0484e
Ashish <ashish@MacBook-Pro.local>
2022-06-16 23:56:03 +0530

    initial project set p

---
+++ Shared/VizurySwiftSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  VizurySwiftSampleApp.swift
+//  Shared
+//
+//  Created by Ashish on 16/06/22.
+//
+
+import SwiftUI
+
+@main
+struct VizurySwiftSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VizuryEventLoggerSdk/VizuryEventLoggerSdk.h
@@ -0,0 +1,18 @@
+//
+//  VizuryEventLoggerSdk.h
+//  VizuryEventLoggerSdk
+//
+//  Created by Ashish on 16/06/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VizuryEventLoggerSdk.
+FOUNDATION_EXPORT double VizuryEventLoggerSdkVersionNumber;
+
+//! Project version string for VizuryEventLoggerSdk.
+FOUNDATION_EXPORT const unsigned char VizuryEventLoggerSdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VizuryEventLoggerSdk/PublicHeader.h>
+
+
